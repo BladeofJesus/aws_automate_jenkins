89bd0fb1c1c8125444e3c5de93bde4654a8e95f6
BladeofJesus <maparole0212@gmail.com>
2022-07-05 13:53:14 -0400

    another commit

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "blade-bucket-project"
-    key = "main"
+    key = "main/terraform.tfstate"
     region = "us-east-1"
     dynamodb_table = "blade-dynamodb-table"
   }
